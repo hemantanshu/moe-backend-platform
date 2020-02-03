a4bc1523ad066f1eaa4cf42dd3420cb6f3a7d88f
Hemant Kumar Sah <hemantanshu@gmail.com>
2020-02-03 23:36:55 +0530

    #comment adding routes

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,21 @@ Route::group(['prefix' => 'api/admin'], function () {
     Route::get('userPreference', 'Sys\UserPreferenceController@getUserPreference');
     Route::post('userPreference', 'Sys\UserPreferenceController@setUserPreference');
     Route::resource('openProperty', 'Sys\OpenPropertyController');
+
+    Route::resource('country', 'Moe\CountryController');
+    Route::resource('zone', 'Moe\ZoneController');
+    Route::resource('district', 'Moe\DistrictController');
+    Route::resource('city', 'Moe\CityController');
+    Route::resource('developer', 'Moe\DeveloperController');
+    Route::resource('developerMember', 'Moe\DeveloperMemberController');
+    Route::resource('developmentAgreement', 'Moe\DevelopmentAgreementController');
+    Route::resource('project', 'Moe\ProjectController');
+    Route::resource('projectDeveloper', 'Moe\ProjectDeveloperController');
+    Route::resource('projectDistrict', 'Moe\ProjectDistrictController');
+    Route::resource('projectLicense', 'Moe\ProjectLicenseController');
+    Route::resource('projectStation', 'Moe\ProjectStationController');
+
+    Route::resource('river', 'Moe\RiverController');
+    Route::resource('station', 'Moe\StationController');
+    Route::resource('userTraining', 'Moe\UserTrainingController');
 });
