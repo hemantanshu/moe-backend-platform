9e9f866ca166579104b4348a5e843a00f7012f69
Hemant Kumar Sah <hemantanshu@gmail.com>
2020-02-05 11:09:03 +0530

    #comment updated routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 Route::get('/', 'TestController@test');
 Route::get('test', 'TestController@test');
 
-Route::resource('country', 'Moe\CountryController');
 Route::post('login', 'Sys\LoginController@validateLogin');
 Route::get('loginCheck', 'Sys\LoginController@checkLogin');
 
@@ -30,3 +29,12 @@ Route::group(['prefix' => 'api/admin'], function () {
     Route::resource('station', 'Moe\StationController');
     Route::resource('userTraining', 'Moe\UserTrainingController');
 });
+
+Route::group(['prefix' => 'api/record'], function () {
+
+    Route::get('userPreference', 'Sys\UserPreferenceController@getUserPreference');
+    Route::post('userPreference', 'Sys\UserPreferenceController@setUserPreference');
+    Route::resource('openProperty', 'Sys\OpenPropertyController');
+});
+
+
