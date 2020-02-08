47b2354ff49bf19f32802d3ee6e1287bc3a1bcb1
Hemant Kumar Sah <hemantanshu@gmail.com>
2020-02-08 22:51:00 +0530

    #comment autologin controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Drivezy\LaravelRecordManager\Jobs\ObserverEventManagerJob;
-use Drivezy\LaravelRecordManager\Library\BusinessRuleManager;
-use Drivezy\LaravelRecordManager\Models\Column;
-use Drivezy\LaravelRecordManager\Models\ModelRelationship;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class TestController
@@ -19,21 +16,7 @@ class TestController extends Controller
      */
     public function test ()
     {
-        //dont let the business rule run when db seeder are executed
-        BusinessRuleManager::$enabled = false;
-
-        //dont let the observer event run when db seeder is running
-        ObserverEventManagerJob::$enabled = false;
-
-        $relationships = ModelRelationship::where('name', 'created_user')->whereNull('source_column_id')->limit(100)->get();
-        foreach ( $relationships as $relationship ) {
-            $column = Column::where('source_type', '07b76506c43824b152745fe7df768486')->where('source_id', $relationship->model_id)->where('name', 'created_by')->first();
-            if ( !$column ) continue;
-
-            $relationship->source_column_id = $column->id;
-            $relationship->save();
-        }
-
+        Auth::loginUsingId(1, true);
         return success_response('seems all good till now!');
     }
 }
