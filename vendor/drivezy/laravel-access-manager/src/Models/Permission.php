d65557cceddb649b7860d99c6e6103beb7e34857
Hemant Kumar Sah <hemantanshu@gmail.com>
2020-02-08 23:25:24 +0530

    #comment added role and permission assignments in the corresponding ones

--- vendor/drivezy/laravel-access-manager/src/Models/Permission.php
+++ vendor/drivezy/laravel-access-manager/src/Models/Permission.php
@@ -9,16 +9,26 @@ use Drivezy\LaravelUtility\Models\BaseModel;
  * Class Permission
  * @package Drivezy\LaravelAccessManager\Models
  */
-class Permission extends BaseModel {
+class Permission extends BaseModel
+{
     /**
      * @var string
      */
     protected $table = 'dz_permissions';
 
     /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function permission_assignments ()
+    {
+        return $this->hasMany(PermissionAssignment::class);
+    }
+
+    /**
      *
      */
-    public static function boot () {
+    public static function boot ()
+    {
         parent::boot();
         self::observe(new PermissionObserver());
     }
--- vendor/drivezy/laravel-access-manager/src/Models/Role.php
+++ vendor/drivezy/laravel-access-manager/src/Models/Role.php
@@ -9,7 +9,8 @@ use Drivezy\LaravelUtility\Models\BaseModel;
  * Class Role
  * @package Drivezy\LaravelAccessManager\Models
  */
-class Role extends BaseModel {
+class Role extends BaseModel
+{
     /**
      * @var string
      */
@@ -18,14 +19,24 @@ class Role extends BaseModel {
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function ip_restrictions () {
+    public function ip_restrictions ()
+    {
         return $this->hasMany(IPRestriction::class, 'source_id')->where('source_type', md5(self::class));
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function role_assignments ()
+    {
+        return $this->hasMany(RoleAssignment::class);
     }
 
     /**
      * Load the observer rule against the model
      */
-    public static function boot () {
+    public static function boot ()
+    {
         parent::boot();
         self::observe(new RoleObserver());
     }
