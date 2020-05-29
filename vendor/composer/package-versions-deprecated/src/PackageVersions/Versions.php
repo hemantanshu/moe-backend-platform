139c0165e2bd112b971738e574cdd4fd1fa5dd30
Hemant Kumar Sah <hemantanshu@gmail.com>
2020-05-29 16:24:09 +0530

    #comment added placment id

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -146,7 +146,7 @@ final class Versions
   'sebastian/version' => '2.0.1@99732be0ddb3361e16ad77b68ba41efc8e979019',
   'theseer/tokenizer' => '1.1.3@11336f6f84e16a720dae9d8e6ed5019efa85a0f9',
   'webmozart/assert' => '1.8.0@ab2cb0b3b559010b75981b1bdce728da3ee90ad6',
-  'laravel/laravel' => 'dev-master@d21fe8d7983b3ffe03cd30d6fe95957957eb6536',
+  'laravel/laravel' => 'dev-master@4e77f1a3990f44bcc8410b62747420cbe5da58f0',
 );
 
     private function __construct()
---
+++ vendor/drivezy/laravel-record-manager/src/Database/Migrations/2020_05_29_162118_add_dz_chart_details_placement_id_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDzChartDetailsPlacementIdTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up ()
+    {
+        Schema::table('dz_chart_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('placement_id')->nullable();
+            $table->foreign('placement_id')->references('id')->on('dz_lookup_values');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down ()
+    {
+        Schema::table('dz_chart_details', function (Blueprint $table) {
+            $table->dropForeign('dz_chart_details_placement_id_foreign');
+            $table->dropColumn('placement_id');
+        });
+    }
+}
